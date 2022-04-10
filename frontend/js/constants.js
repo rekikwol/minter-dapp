32e71ec4fc095d2cf570dafba5d8009855029005
rekikwol <rekikwoldemichael@icloud.com>
2022-04-09 23:27:55 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x22C6D2bA03f59c9C463466F2FFeeEb41d285208D"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
